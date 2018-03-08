a641c25964b4451f880d1911218bf63c75c9deaf
vcribb <vcribb@stuy.edu>
2018-03-08 15:23:45 -0500

    started basics of both bronze and silver

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,5 +1,23 @@
-public static int bronze(String filename){
-}
+public class USACO{
 
-public static int silver(String filename){
+    public static int bronze(String filename){
+	File text = new File(filename);
+        Scanner inf = new Scanner(text);
+	String l = "";
+        while(inf.hasNextLine()){
+	    l += inf.nextLine();
+	}
+	
+    }
+
+    public static int silver(String filename){
+	File text = new File(filename);
+	Scanner inf = new Scanner(text);
+	String l = "";
+	while(inf.hasNextLine()){
+	    l += inf.nextLine();
+	}
+	//make the char[][] array
+	//initialize int[][]s
+    }
 }
