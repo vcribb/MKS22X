ecccf54bd4b8b45907e529706faf82f00c6fc2d4
Vivian <vcribb@stuy.edu>
2018-03-12 21:55:25 -0400

    wrote a basic swap method

--- 06QuickSelect/QuickSelect.java
+++ 06QuickSelect/QuickSelect.java
@@ -1,7 +1,15 @@
 public class QuickSelect{
 
-    public int partition(int[] data, int start, int end){
+    public static int partition(int[] data, int start, int end){
+	
+
 	return 0;
+    }
+
+    public static void swap(int[] nums, int x, int y){
+	int temp = nums[x];
+	nums[x] = nums[y];
+	nums[y] = temp;
     }
 
 }
