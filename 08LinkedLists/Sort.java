b52d18933a5502e8d93760b37adaaaaae2a948a4
vcribb <vcribb@stuy.edu>
2018-04-13 15:35:22 -0400

    basics of radix

--- 08LinkedLists/Sort.java
+++ 08LinkedLists/Sort.java
@@ -1,6 +1,18 @@
-public class Sort{
+public class Sort extends MyLinkedList{
 
     public static void radixsort(MyLinkedListImproved<Integer> data){
+
+	//create bucket array
+	//@SuppressWarnings("unchecked")
+	//MyLinkedList<Integer>[] data = new MyLinkedList[10]
+
+	//find maximum element
+	//find power of ten you need
+	
+	//loop through array and sort integers into buckets
+	//merge linked lists
+	//repeat until done
+	
     }
     
 }
