58c9e310cd3a4fc53a9c8ff7edd275c91ff6476f
Vivian <vcribb@stuy.edu>
2018-04-14 07:51:01 -0400

    extended to negative numbers

--- 08LinkedLists/Sort.java
+++ 08LinkedLists/Sort.java
@@ -1,6 +1,26 @@
 public class Sort extends MyLinkedListImproved{
 
     public static void radixsort(MyLinkedListImproved<Integer> data){
+	MyLinkedListImproved<Integer> pos = new MyLinkedListImproved<>();
+	MyLinkedListImproved<Integer> neg = new MyLinkedListImproved<>();
+
+	for (Integer x : data){
+	    if (x > 0){
+		pos.add(x);
+	    }
+	    else{
+		neg.add(x);
+	    }
+	}
+
+	radixpositive(pos);
+	radixnegative(neg);
+	neg.extend(pos);
+	data.first = neg.first;
+	data.last = neg.last;
+    }
+
+    private static void radixpositive(MyLinkedListImproved<Integer> data){
 
 	//create bucket array
 	@SuppressWarnings("unchecked")MyLinkedListImproved<Integer>[] digits = new MyLinkedListImproved[10];
@@ -31,7 +51,38 @@ public class Sort extends MyLinkedListImproved{
 	}
     }
 
-    public static int log10(int x){
+    private static void radixnegative(MyLinkedListImproved<Integer> data){
+
+	//create bucket array
+	@SuppressWarnings("unchecked")MyLinkedListImproved<Integer>[] digits = new MyLinkedListImproved[10];
+	for (int x = 0; x < 10; x++){
+	    digits[x] = new MyLinkedListImproved<Integer>();
+	}
+
+	//find maximum element
+	int max = -1 * data.get(data.min());
+	int log = log10(max);
+        
+	//loop through array and sort integers into buckets
+	for (int counter = 0; counter < log; counter++){
+	    for (Integer x : data){
+		int digit = (-1 * x / ((int)java.lang.Math.pow(10, counter))) % 10;
+		digits[digit].add(x);
+	    }
+
+	    //merge lists
+	    data.clear();
+	    data.add(0);
+	    for (int x = 9; x >= 0; x--){
+		if (digits[x].size() != 0){
+		    data.extend(digits[x]);
+		}
+	    }
+	    data.remove(0);
+	}
+    }
+
+    private static int log10(int x){
 	int ans = 0;
 	while (x > 0){
 	    x /= 10;
@@ -41,9 +92,8 @@ public class Sort extends MyLinkedListImproved{
     }
 
     public static void main(String[] args){
-	System.out.println(log10(100));
 	MyLinkedListImproved<Integer> m = new MyLinkedListImproved<>();
-	for (int x = 50; x > 0; x-=2){
+	for (int x = 50; x >= -50; x-=2){
 	    m.add(x);
 	}
 	radixsort(m);
