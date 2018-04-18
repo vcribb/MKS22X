9d10c8377cfb0a19a4cc4f5e9319080a16d9ec5d
vcribb <vcribb@stuy.edu>
2018-04-18 15:09:18 -0400

    initialized

---
+++ 09StackCalc/Calculator.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Calculator{
+
+    private class Stack{
+
+	private LinkedList list;
+
+	public Stack(){
+	    list = new LinkedList<>();
+	}
+	
+    }
+    
+    public static double eval(String s){
+	return 0;
+    }
+
+}
