0a2c18cc6473d64b2a076843fdf968d6e2d3438b
Vivian <vcribb@stuy.edu>
2018-04-19 20:13:28 -0400

    wrote eval and tested

--- 09StackCalc/Calculator.java
+++ 09StackCalc/Calculator.java
@@ -1,21 +1,45 @@
 import java.util.*;
+import java.io.*;
 
 public class Calculator{
-
-    private class Stack{
-
-	private LinkedList list;
-
-	public Stack(){
-	    list = new LinkedList<>();
-	}
-	
-    }
     
     public static double eval(String s){
-	//split string on the spaces
-	
-	return 0;
+	String[] list = s.split("\\s+");
+	@SuppressWarnings("unchecked")
+	LinkedList<String> stack = new LinkedList();
+	for (int x = 0; x < list.length; x++){
+	    try{
+		double temp = Double.parseDouble(list[x]);
+		stack.push(temp+"");
+	    }
+	    catch (NumberFormatException e){
+		double b = Double.parseDouble(stack.pop());
+		double a = Double.parseDouble(stack.pop());
+		if (list[x].equals("+")){
+		    stack.push(a + b + "");
+		}
+		if (list[x].equals("-")){
+		    stack.push(a - b + "");
+		}
+		if (list[x].equals("*")){
+		    stack.push(a * b + "");
+		}
+		if (list[x].equals("/")){
+		    stack.push(a / b + "");
+		}
+		if (list[x].equals("%")){
+		    stack.push(a % b + "");
+		}
+	    }
+	}
+	return Double.parseDouble(stack.pop());
+    }
+    
+    public static void main(String[] args){
+	System.out.println(eval("6 6 +"));
+	System.out.println(eval("6 6 + 12 *"));
+	System.out.println(eval("11 3 - 4 + 2.5 *"));
+	System.out.println(eval("8 2 + 99 9 - * 2 +"));
     }
 
 }
